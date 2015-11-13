cd66bbb8fb2fd31c55b135c98fd0fffb4f00bd43
Trotyl Yu <yzj1995@vip.qq.com>
2015-11-13 16:36:02 +0800

    Change application structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,12 @@ import { bootstrap, Component } from 'angular2/angular2';
 
 @Component({
     selector: 'ng-demo',
-    template: '<h1>This is the app component.</h1>'
+    template: `
+    <h1>This is the app component.</h1>
+    `
 })
 export class AppComponent {
 
 }
 
-bootstrap(AppComponent);
+//bootstrap(AppComponent);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { bootstrap } from 'angular2/angular2';
-import { AppComponent } from "./components/app";
+import { AppComponent } from "app/app";
 
 
 bootstrap(AppComponent);
