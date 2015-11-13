e6e47707dbc5ffce439f7acf39496c433f1322dc
Trotyl Yu <yzj1995@vip.qq.com>
2015-11-13 16:47:47 +0800

    Add style from bootstrap.

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,7 +4,13 @@ import { bootstrap, Component } from 'angular2/angular2';
 @Component({
     selector: 'ng-demo',
     template: `
-    <h1>This is the app component.</h1>
+    <div class="container">
+        <div class="jumbotron">
+            <h1>This is the app component.</h1>
+            <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+            <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+        </div>
+    </div>
     `
 })
 export class AppComponent {
