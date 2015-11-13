8fbc6dd93a28d01bd9a4dbc8107db4555e3a059c
Trotyl Yu <yzj1995@vip.qq.com>
2015-11-13 14:59:23 +0800

    Minimal runnable product.

---
+++ components/app.ts
@@ -0,0 +1,12 @@
+import { bootstrap, Component } from 'angular2/angular2';
+
+
+@Component({
+    selector: 'ng-demo',
+    template: '<h1>This is the app component.</h1>'
+})
+export class AppComponent {
+
+}
+
+bootstrap(AppComponent);
---
+++ index.ts
@@ -0,0 +1,5 @@
+import { bootstrap } from 'angular2/angular2';
+import { AppComponent } from "./components/app";
+
+
+bootstrap(AppComponent);
